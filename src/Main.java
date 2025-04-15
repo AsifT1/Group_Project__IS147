6299434fbfe91a54f8589fcbc5792a4f8cd5b8ac
AsifT1 <AsifT1@umbc.edu>
2025-04-14 21:17:22 -0400

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    /**
+     * This a main class to test Person.java
+     *
+     * @author Asif Tanjim
+     * @version 1.0
+     * @see Main
+     */
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome momo!");
+
+        /**
+         * To set a person name
+         *
+         * @param Asif a new person  pass the string variable <code>String</code>
+         */
+
+        /**
+         * This is a construction to initialize person object
+         *
+         * @param Asif inital person name
+         */
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
@@ -12,6 +32,8 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("This is Raihan A.");
             System.out.println("Hello");
+
+
         }
     }
 }
