19e8f8a8ae593a013b52db65a21cec914c2991d9
miles-ph9 <milesmouni@gmail.com>
2025-04-14 23:32:50 -0400

    Add files via upload

---
+++ src/Car_Project.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Car_Project {
+
+
+    public static void main(String[] args) {
+
+        Scanner carModel = new Scanner(System.in);
+
+        System.out.println("Hello customer! Would you be looking for a specific model of vehicle today! : ");
+
+
+
+
+    }
+}
